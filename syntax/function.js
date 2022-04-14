7c8f5a034adb721fbb838da9034e49ded7773e39
Dani <jungwoo.oh0@gmail.com>
2022-04-14 11:32:51 +0200

    fonction과 main의 가독성

---
+++ syntax/function.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6)); //rounds는 반올림
+console.log(Math.round(1.4)); 
+
+function sum(first,seconde){ //parameter
+    return first + seconde;//return 즉시 함수 정지
+}
+
+console.log(sum(1,2)); //각각 argument라고함
