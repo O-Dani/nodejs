1fc1885ada54af2198fb4fdfe12e60ffd32f77c3
Dani <jungwoo.oh0@gmail.com>
2022-04-15 02:30:23 +0200

    묘듈의 활용 (main.js)

--- main.js
+++ main.js
@@ -1,40 +1,8 @@
-var http = require('http'); // 클라이언트에서 요청이 오면 이 함수가 실행된다.
+var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-const path = require('path');
-
-var template = {
-  HTML:
-  function(title, list, body,control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `},
-    
-  list:function (filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list+'</ul>';
-    return list;
-  }
-
-}
+var template = require('./lib/template.js');
  
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -45,20 +13,11 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-        /*
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, 
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-            );
-          response.writeHead(200);
-          response.end(template);*/
-
           var list = template.list(filelist);
-          var html = template.HTML(title, list, 
+          var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>`
-            );
+          );
           response.writeHead(200);
           response.end(html);
         });
@@ -67,14 +26,14 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = template.list(filelist);
-            var html = template.HTML(title, list, 
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a> 
-            <a href="/update?id=${title}">update</a>
-            <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <input type="submit" value="delete">
-            </form>`
+            var html = template.HTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
             );
             response.writeHead(200);
             response.end(html);
@@ -95,7 +54,7 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, ``);
+        `, '');
         response.writeHead(200);
         response.end(html);
       });
@@ -107,38 +66,37 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
           var post = qs.parse(body);
           var title = post.title;
-          var description = post.description
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){});//에러가 있을경우 처리하는 방법
-            response.writeHead(302,{Location :`/?id=${title}`});//성공시 200 , 실패시 404, 302는 redirection
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
+          })
       });
-      
-    } else if (pathname ==="/update"){
+    } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
-          var html = template.HTML(title, list, 
-          `
-          <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value=${title}></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          var html = template.HTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
           );
           response.writeHead(200);
           response.end(html);
         });
       });
-
-    }else if (pathname === '/update_process'){ //글수정 파일명 변경,내용 저장
+    } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -147,15 +105,15 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var id = post.id;
           var title = post.title;
-          var description = post.description
+          var description = post.description;
           fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){});
-            response.writeHead(302,{Location :`/?id=${title}`});
-            response.end();
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
           });
       });
-
-    }else if (pathname === '/delete_process'){ //글 삭제기능
+    } else if(pathname === '/delete_process'){
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -164,17 +122,13 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var id = post.id;
           fs.unlink(`data/${id}`, function(error){
-            response.writeHead(302,{Location :`/`}); //home 으로 보내기
+            response.writeHead(302, {Location: `/`});
             response.end();
-
-          });
+          })
       });
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
- 
- 
- 
 });
 app.listen(3000);
